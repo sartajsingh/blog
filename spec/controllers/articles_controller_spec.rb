75fb959a00cf7e8458af06852c42abf371487b78
Sartaj Singh <srtaj.sngh@gmail.com>
2015-06-26 16:34:39 +0530

    controller specs

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,31 +2,37 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, :type => :controller do
 
-describe "Articles Controller" do
   describe "index" do
     it "returns correct number of articles saved" do
-     get :index
+      Article.create( {:title => "onetwo", :text => "asasas"} )
+      Article.create( title: "onwtwo", text: "asasas" )
+      get :index
+      expect(assigns(:articles).count).to eq 2
     end
+
   end
+
 
   describe "show" do
     it "returns an article whose id is passed" do
 
-      article_one = Article.new( {:title => "qqqqqq", :text => "asasas"} )
-      article_one.save
+      article_one = Article.create( {:title => "qqqqqq", :text => "asasas"} )
       get :show , :id => article_one
       expect(article_one.title).to eq("qqqqqq")
     end
   end
 
   describe 'new' do
-    it "" do
+    it "should initialize a new article object" do
+      get :new
 
     end
   end
 
   describe 'edit' do
     it 'finds and edits the article' do
+
+
     end
   end
 
@@ -39,10 +45,18 @@ describe "Articles Controller" do
   end
 
   describe "update" do
-    it "upadtes an existing article" do
-
+    it "updates an existing article" do
     end
+    it "renders the edit template" do
+      article_one = Article.create( {:title => "qqqqqq", :text => "asasas"} )
+      attr = {:title => "new title", :text => "new content" }
+      put :update, :id => article_one.id, :article => attr
+      article_one.reload
+      expect(article_one.title).to eq attr[:title] 
+      expect(article_one.text).to eq attr[:text] 
+    end
+
+
   end
 
-end
 end
